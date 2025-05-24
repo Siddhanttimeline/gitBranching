9a4cc650ccbb019ef335862dd9143e19a347b6a9
Siddhanttimeline <siddhanttimeline9@gmail.com>
2025-05-24 23:45:43 +0530

    add sout 2

--- src/main/java/com/slackapp/slackApp/SlackAppApplication.java
+++ src/main/java/com/slackapp/slackApp/SlackAppApplication.java
@@ -9,6 +9,7 @@ public class SlackAppApplication {
 	public static void main(String[] args) {
 		System.out.println("Main App");
 		SpringApplication.run(SlackAppApplication.class, args);
+		System.out.println("App");
 	}
 
 }
