691602ab75ea5b28d65f5c987e6bba320fbbf3ef
Siddhanttimeline <siddhanttimeline9@gmail.com>
2025-07-16 02:20:29 +0530

    .github

--- src/main/java/com/slackapp/slackApp/SlackAppApplication.java
+++ src/main/java/com/slackapp/slackApp/SlackAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SlackAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SlackAppApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SlackAppApplication.class, args);
+  }
 }
--- src/test/java/com/slackapp/slackApp/SlackAppApplicationTests.java
+++ src/test/java/com/slackapp/slackApp/SlackAppApplicationTests.java
@@ -2,12 +2,15 @@ package com.slackapp.slackApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = SlackAppApplication.class)
+@TestPropertySource(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class SlackAppApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
+
